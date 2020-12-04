53dedfca4323fa0fd444ac8b47fb3b5b336d6d63
msmetalurgica <71799540+msmetalurgica@users.noreply.github.com>
2020-12-04 13:37:30 -0300

    Update index.js

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -6,7 +6,7 @@ const App = () => (
   <Mapa>
     <iframe
       title="Localização"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3687.253454118339!2d-46.788955785411574!3d-22.45710792762438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c90376f87272e1%3A0x2e6ec872427956af!2sMS%20Metal%C3%BArgica!5e0!3m2!1spt-BR!2sbr!4v1570013998955!5m2!1spt-BR!2sbr"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3687.4115016060905!2d-46.80488768564513!3d-22.451165585245374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c9025cccca9a97%3A0x58c01d70cd3a21cc!2sR.%20Ant%C3%B4nio%20Cavalaro%2C%20137%20-%20Jardim%20Paraiso%2C%20Itapira%20-%20SP%2C%2013973-474!5e0!3m2!1spt-BR!2sbr!4v1607099646898!5m2!1spt-BR!2sbr"
       width="100%"
       height="345"
       frameBorder="0"
